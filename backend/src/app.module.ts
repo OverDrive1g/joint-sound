6c9edeb2b7c34e558590d8e8426936ae1e351c6c
Vlad Bedilo <v.overdrive@gmail.com>
2020-11-14 01:04:20 +0300

    feat(backend): add env config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import {TypeOrmModule } from '@nestjs/typeorm'
 import {QueuePersustenceModule} from './modules/queue-persistence/queue-persistence.module';
 import {QueueWebModule} from './modules/queue-web/queue-web.module';
@@ -6,9 +7,10 @@ import {QueueWebModule} from './modules/queue-web/queue-web.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type:'better-sqlite3',
-      database: '/tmp/joint-sound.sqlite',
+      database: process.env.DATABASE_PATH,
       logging:true,
       autoLoadEntities:true,
       migrations:[__dirname, '/migrations/**/*.ts'],
