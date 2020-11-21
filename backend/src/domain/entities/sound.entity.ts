13a83b51eadbf96ae3bb7e1e7d86562f043c0504
Vlad Bedilo <v.overdrive@gmail.com>
2020-11-21 03:24:54 +0300

    dev(backend): impliment skip sound

--- backend/src/domain/entities/sound.entity.ts
+++ backend/src/domain/entities/sound.entity.ts
@@ -1,3 +1,19 @@
 export class SoundEntity {
-  constructor(private readonly name: string, private readonly source: string) {}
+ constructor(
+   private readonly _name: string, 
+   private readonly _source: string,
+   private readonly _startFrom: number
+ ) {}
+
+ get name(){
+   return this._name;
+ }
+ get source(){
+   return this._source;
+ }
+
+ get startFrom(){
+   return this._startFrom;
+ }
+
 }
--- backend/src/domain/services/skip-sound-from-queue.service.ts
+++ backend/src/domain/services/skip-sound-from-queue.service.ts
@@ -2,11 +2,38 @@ import {Either, right, left} from '@sweet-monads/either'
 
 import {SkipSoundFromQueueUseCase, SkipSoundError} from '../ports/in/skip-sound-from-queue/skip-sound-from-queue.use-case';
 import {SkipSoundFromQueueCommand} from '../ports/in/skip-sound-from-queue/skip-sound-from-queue.command';
+import {LoadQueuePort} from '../ports/out/load-queue.port';
+import {UpdateQueuePort} from '../ports/out/update-queue.port';
+import {QueueEntity} from '../entities/queue.entity';
 
 export class SkipSoundFromQueue implements SkipSoundFromQueueUseCase {
+
+  constructor(
+   private readonly _loadQueue: LoadQueuePort,
+   private readonly _updateQueue:UpdateQueuePort,
+  ){} 
+
+  async execute(command:SkipSoundFromQueueCommand):Promise<Either<SkipSoundError, void>>{
+    //TODO: check user permission
+    const loadResult = await this._loadQueue.loadQueue(command.queueId)
+    if(loadResult.isLeft()){
+      return left(new SkipSoundError("Can't load queue from storage"))
+    }
+    
+    const queue:QueueEntity = loadResult.value
+    const soundIndex = queue.sounds.findIndex(sound=>sound.name === command.sound.name)
+    if(soundIndex === -1){
+      return left(new SkipSoundError("Can't found sound in queue"))
+    }
   
-  async execute(command):Promise<Either<SkipSoundError, void>>{
-    throw "";
+   queue.sounds.splice(soundIndex,1)
+
+   const updateResult = await this._updateQueue.updateQueue(queue)
+   if(updateResult.isLeft()){
+     return left(new SkipSoundError("Can't update queue"))
+   }
+
+   return right((()=>{return})());//FIXME
   }
 
 }
