5955c8c1f4849b776a78c811034ea868718ac939
Vlad Bedilo <v.overdrive@gmail.com>
2020-12-01 01:56:17 +0300

    dev(backend): add method for sub/unsub

--- backend/src/modules/websocket/test.gateway.ts
+++ backend/src/modules/websocket/test.gateway.ts
@@ -5,7 +5,7 @@ import {
   WebSocketServer,
   OnGatewayInit,
   OnGatewayConnection,
-  OnGatewayDisconnect,
+  OnGatewayDisconnect, ConnectedSocket
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
@@ -14,6 +14,7 @@ export class TestGateway implements OnGatewayInit,OnGatewayConnection,OnGatewayD
 
   @WebSocketServer()
   server: Server;
+  subscriptions = [] as {id:string,queue:string,intervalId:NodeJS.Timeout}[];
 
   afterInit() {
     console.log('Ws init');
@@ -27,14 +28,41 @@ export class TestGateway implements OnGatewayInit,OnGatewayConnection,OnGatewayD
   }
   
   @SubscribeMessage('subscribeToQueue')
-  subscribeToQueue(@MessageBody() data: string): string {
+  subscribeToQueue(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket
+  ): string {
     console.log('subscribeToQueue', data)
-    return ;
+    this.subscribeClient(client, data)
+    return 'ok';
   }
   @SubscribeMessage('unsubscribeToQueue')
-  unsubscribeToQueue(@MessageBody() data: string): string {
+  unsubscribeFromQueue(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket
+  ): string {
     console.log('unsubscribeToQueue', data)
+    this.unsubscribeClient(client, data)
     return ;
   }
 
+
+  subscribeClient(client:Socket,queue:string){
+    const intervalId = setInterval(()=>{
+      client.emit('next-track', JSON.stringify({trackId:1}))
+    }, 5000)
+
+    this.subscriptions.push({
+      id:client.id,
+      queue,
+      intervalId
+    })
+  }
+
+  unsubscribeClient(client:Socket,queue:string){
+    let foundSubscription = this.subscriptions.find(itm=>itm.id === client.id && itm.queue === queue)
+    if(foundSubscription){
+      clearInterval(foundSubscription.intervalId)
+    }
+  }
 }
